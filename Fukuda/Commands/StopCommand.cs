1f7b7694efa74d9e46e9738db5facb38094285d6
flustix <me@flux.moe>
2024-11-20 16:25:10 +0100

    only allow /stop when in the same vc

--- Fukuda/Commands/StopCommand.cs
+++ Fukuda/Commands/StopCommand.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
 using HotelLib;
 using HotelLib.Commands;
 using HotelLib.Utils;
@@ -12,6 +13,24 @@ public class StopCommand : SlashCommand
 
     public override async Task Handle(HotelBot bot, DiscordInteraction interaction)
     {
+        var member = await interaction.Guild.GetMemberAsync(interaction.User.Id) ?? throw new ArgumentNullException();
+        var vc = member.VoiceState?.Channel;
+
+        var vnext = bot.Client.GetVoiceNext();
+        var connection = vnext.GetConnection(interaction.Guild);
+
+        if (connection is null)
+        {
+            await interaction.Reply("Not connected to any channel!", true);
+            return;
+        }
+
+        if (vc is null || vc.Id != connection.TargetChannel.Id)
+        {
+            await interaction.Reply("You are not in the same channel!", true);
+            return;
+        }
+
         PlaylistManager.StopAll();
         await interaction.Reply("Stopped playback.");
     }
